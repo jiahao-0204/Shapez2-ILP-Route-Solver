ca5ff99a22542e5155d537f13165675aa67ff787
Jiahao Wang <github@jiahao.wang>
2025-04-27 12:39:09 +0100

    feat(swapper): component and example

---
+++ components/Swapper.py
@@ -0,0 +1,79 @@
+from components.Component import Component
+from constants import OFFSET, Node, Direction, Amount
+import matplotlib.pyplot as plt
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Router import Router
+
+class SwapperComponent(Component):
+    def __init__(self, node: Node, direction: Direction, secondary_direction: Direction, amount: Amount):
+        super().__init__()
+        
+        # node and direction
+        self.node1 = node
+        self.direction = direction
+        self.secondary_direction = secondary_direction
+        self.amount = amount
+        self.node2 = (self.node1[0] + self.secondary_direction[0], self.node1[1] + self.secondary_direction[1])
+        self.source1 = (self.node1[0] + self.direction[0], self.node1[1] + self.direction[1])
+        self.source2 = (self.node2[0] + self.direction[0], self.node2[1] + self.direction[1])
+        self.input1 = (self.node1[0] - self.direction[0], self.node1[1] - self.direction[1])
+        self.input2 = (self.node2[0] - self.direction[0], self.node2[1] - self.direction[1])
+
+        # x and y
+        self.x, self.y = self.node1
+        self.dx, self.dy = self.direction
+        self.x2, self.y2 = self.node2
+        self.dx2, self.dy2 = self.secondary_direction
+        self.nx, self.ny = self.source1
+        self.nx2, self.ny2 = self.source2
+
+    def draw(self, ax: plt.Axes):
+        # compute cutter dimensions
+        margin = 0.2
+        ll_x = min(self.x, self.x2) + margin
+        ll_y = min(self.y, self.y2) + margin
+        width  = abs(self.x2 - self.x) + 1 - 2 * margin       # +1 because each node is 1×1
+        height = abs(self.y2 - self.y) + 1 - 2 * margin
+
+        # draw rectangle
+        rect = plt.Rectangle((ll_x, ll_y), width, height, facecolor='grey', edgecolor='black', linewidth=1.2, zorder=1)
+        ax.add_patch(rect)
+
+        # draw triangles
+        if self.direction == (0, 1):
+            marker = '^'
+        elif self.direction == (0, -1):
+            marker = 'v'
+        elif self.direction == (1, 0):
+            marker = '>'
+        elif self.direction == (-1, 0):
+            marker = '<'
+        ax.scatter(self.x + OFFSET, self.y + OFFSET, c='black', marker=marker, s=80, edgecolors='black', zorder = 2)
+        ax.scatter(self.x2 + OFFSET, self.y2 + OFFSET, c='grey', marker=marker, s=80, edgecolors='black', zorder = 2)
+
+        # draw lines
+        ax.plot([self.x + OFFSET, self.nx + OFFSET], [self.y + OFFSET, self.ny + OFFSET], c='black', zorder=0)
+        ax.plot([self.x2 + OFFSET, self.nx2 + OFFSET], [self.y2 + OFFSET, self.ny2 + OFFSET], c='black', zorder=0)
+
+    def add_constraints(self, router: "Router"):
+        router.add_sink_node_constraints(self.node1, self.input1, self.direction)
+        router.add_sink_node_constraints(self.node2, self.input2, self.direction)
+        router.add_source_node_constraints(self.source1, self.direction)
+        router.add_source_node_constraints(self.source2, self.direction)
+
+    def get_io(self, ith):
+        if ith == 0:
+            return (self, self.node1, self.amount)
+        elif ith == 1:
+            return (self, self.node2, self.amount)
+        elif ith == 2:
+            return (self, self.source1, self.amount)
+        elif ith == 3:
+            return (self, self.source2, self.amount)
+        else:
+            raise ValueError("Invalid index for get_io")
+    
+    def get_nodes(self):
+        return [self.node1, self.node2]
---
+++ examples/1x2_swapper.py
@@ -0,0 +1,74 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from components.Start import StartComponent
+from components.Goal import GoalComponent
+from components.Swapper import SwapperComponent
+from Router import Router
+from constants import *
+from typing import List
+
+if __name__ == "__main__":
+    starts1: List[StartComponent] = [
+        StartComponent((6, 0), (0, 1), 4),
+        StartComponent((7, 0), (0, 1), 4),
+        StartComponent((8, 0), (0, 1), 4),
+        StartComponent((9, 0), (0, 1), 4),
+    ]
+
+    starts2: List[StartComponent] = [
+        StartComponent((26, 0), (0, 1), 4),
+        StartComponent((27, 0), (0, 1), 4),
+        StartComponent((28, 0), (0, 1), 4),
+        StartComponent((29, 0), (0, 1), 4),
+    ]
+    
+    goals1: List[GoalComponent] = [
+        GoalComponent((6, 15), (0, 1), 4),
+        GoalComponent((7, 15), (0, 1), 4),
+        GoalComponent((8, 15), (0, 1), 4),
+        GoalComponent((9, 15), (0, 1), 4),
+    ]
+
+    goals2: List[GoalComponent] = [
+        GoalComponent((26, 15), (0, 1), 4),
+        GoalComponent((27, 15), (0, 1), 4),
+        GoalComponent((28, 15), (0, 1), 4),
+        GoalComponent((29, 15), (0, 1), 4),
+    ]
+
+    swappers: List[SwapperComponent] = [
+        SwapperComponent((2, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((5, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((6, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((9, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((10, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((13, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((14, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((17, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((18, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((21, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((22, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((25, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((26, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((29, 7), (0, 1), (-1, 0), 1),
+        SwapperComponent((30, 7), (0, 1), (1, 0), 1),
+        SwapperComponent((33, 7), (0, 1), (-1, 0), 1),
+    ]
+    
+    # create router
+    router = Router()
+    router.initialize_board(width = 36, height = 16, jump_distances = [1, 2, 3, 4], num_nets = 4)
+    router.add_components(starts1)
+    router.add_components(starts2)
+    router.add_components(goals1)
+    router.add_components(goals2)
+    router.add_components(swappers)
+    router.generate_and_add_borders()
+    router.add_net([c.get_io() for c in starts1], [c.get_io(0) for c in swappers])
+    router.add_net([c.get_io() for c in starts2], [c.get_io(1) for c in swappers])
+    router.add_net([c.get_io(2) for c in swappers], [c.get_io() for c in goals1])
+    router.add_net([c.get_io(3) for c in swappers], [c.get_io() for c in goals2])
+    router.draw_board()
+    router.solve(timelimit = NO_TIME_LIMIT, option = MIPFOCUS_FEASIBILITY)
