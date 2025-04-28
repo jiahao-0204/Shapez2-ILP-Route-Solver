f69002c2084ea1586f4d895195277dedfba0c082
Jiahao Wang <github@jiahao.wang>
2025-04-28 23:59:01 +0100

    feat(one set is_edge_used)

--- Router.py
+++ Router.py
@@ -72,32 +72,38 @@ class Router:
 
         # net specific variables
         self.edge_flow_value: Dict[int, Dict[Edge, Var]] = defaultdict(lambda: defaultdict(Var))
-        self.is_edge_used: Dict[int, Dict[Edge, Var]] = defaultdict(lambda: defaultdict(Var))
         self.node_in_flow_value_expr: Dict[int, Dict[Node, LinExpr]] = defaultdict(lambda: defaultdict(LinExpr))
         self.node_out_flow_value_expr: Dict[int, Dict[Node, LinExpr]] = defaultdict(lambda: defaultdict(LinExpr))
-        self.is_node_used_by_belt: Dict[int, Dict[Node, Var]] = defaultdict(lambda: defaultdict(Var))
         for i in range(self.num_nets):
             for edge in self.all_edges:
                 # edge flow value
                 self.edge_flow_value[i][edge] = self.model.addVar(name = f"edge_flow_value_{i}_{edge}", vtype=GRB.INTEGER, lb=0, ub=FLOW_CAP)
                 self.edge_flow_value[i][edge].setAttr("BranchPriority", FLOW_PRIORITY)
 
-                # is edge used (dynamic variable)
-                self.is_edge_used[i][edge] = self.model.addVar(name=f"edge_{i}_{edge}", vtype=GRB.BINARY)
-                self.is_edge_used[i][edge].setAttr("BranchPriority", EDGE_PRIORITY)
-                self.model.addGenConstrIndicator(self.is_edge_used[i][edge], True, self.edge_flow_value[i][edge] >= 1)
-                self.model.addGenConstrIndicator(self.is_edge_used[i][edge], False, self.edge_flow_value[i][edge] == 0)
-
                 # in flow and out flow values
                 self.node_in_flow_value_expr[i][edge[1]].addTerms(1, self.edge_flow_value[i][edge])
                 self.node_out_flow_value_expr[i][edge[0]].addTerms(1, self.edge_flow_value[i][edge])
-        for i in range(self.num_nets):
-            for node in self.all_nodes:
-                # is node used by belt (dynamic variable)
-                node_belt_edges_bool_list = [self.is_edge_used[i][edge] for edge in self.node_related_belt_edges[node]]
-                self.is_node_used_by_belt[i][node] = self.model.addVar(name=f"node_{i}_{node}", vtype=GRB.BINARY)
+        
+        # one set of is_edge_used
+        self.is_edge_used: Dict[Edge, Var] = defaultdict(Var)
+        for edge in self.all_edges:
+            self.is_edge_used[edge] = self.model.addVar(name=f"edge_{edge}", vtype=GRB.BINARY)
+            self.is_edge_used[edge].setAttr("BranchPriority", EDGE_PRIORITY)
 
-                self.model.addGenConstrOr(self.is_node_used_by_belt[i][node], node_belt_edges_bool_list)
+            # todo
+            # # if an edge is used, one of the flow value must be >= 1
+            # self.model.addGenConstrIndicator(self.is_edge_used[edge], True, self.edge_flow_value[i][edge] >= 1)
+
+            # if the edge is not used, all of the flow value must be 0
+            for i in range(self.num_nets):
+                self.model.addGenConstrIndicator(self.is_edge_used[edge], False, self.edge_flow_value[i][edge] == 0)
+
+        self.is_node_used_by_belt: Dict[Node, Var] = defaultdict(Var)
+        for node in self.all_nodes:
+            # is node used by belt (dynamic variable)
+            node_belt_edges_bool_list = [self.is_edge_used[edge] for edge in self.node_related_belt_edges[node]]
+            self.is_node_used_by_belt[node] = self.model.addVar(name=f"used_by_belt_{node}", vtype=GRB.BINARY)
+            self.model.addGenConstrOr(self.is_node_used_by_belt[node], node_belt_edges_bool_list)
         
         # constraints
         self.add_flow_max_value_constraints()
@@ -112,34 +118,44 @@ class Router:
                 self.model.addConstr(self.node_out_flow_value_expr[i][node] <= FLOW_CAP)
 
     def add_belt_pad_net_overlap_constraints(self):
-        # between belts / pads in different nets
+        # between belts / pads
         for node in self.all_nodes:
             list_of_things_using_node = []
-            for i in range(self.num_nets):
-                list_of_things_using_node += [self.is_node_used_by_belt[i][node]]
-                list_of_things_using_node += [self.is_edge_used[i][edge] for edge in self.node_related_starting_pad_edges[node]]
-                list_of_things_using_node += [self.is_edge_used[i][edge] for edge in self.node_related_landing_pad_edges[node]]
+
+            list_of_things_using_node += [self.is_node_used_by_belt[node]]
+            list_of_things_using_node += [self.is_edge_used[edge] for edge in self.node_related_starting_pad_edges[node]]
+            list_of_things_using_node += [self.is_edge_used[edge] for edge in self.node_related_landing_pad_edges[node]]
             
             # constraint: at most one thing can use a node
             self.model.addConstr(quicksum(list_of_things_using_node) <= 1)
+        
+        # between different nets
+        self.is_node_used_by_net: Dict[int, Dict[Node, Var]] = defaultdict(lambda: defaultdict(Var))
+        for node in self.all_nodes:
+            for i in range(self.num_nets):
+                self.is_node_used_by_net[i][node] = self.model.addVar(name=f"used_by_net_{i}_{node}", vtype=GRB.BINARY)
+                self.model.addGenConstrIndicator(self.is_node_used_by_net[i][node], True, self.node_in_flow_value_expr[i][node] + self.node_out_flow_value_expr[i][node] >= 1)
+                self.model.addGenConstrIndicator(self.is_node_used_by_net[i][node], False, self.node_in_flow_value_expr[i][node] + self.node_out_flow_value_expr[i][node] == 0)
+            
+            # at most one net can use a node
+            self.model.addConstr(quicksum(self.is_node_used_by_net[i][node] for i in range(self.num_nets)) <= 1)
     
     def add_pad_direction_constraints(self):
         # for each edge, if the edge is used, then the end node must not have jump edge at different direction
-        for i in range(self.num_nets):
-            for edge in self.all_edges:
-                u, v, direction = edge
-                
-                # no landing pad if edge is used
-                for jump_edge in self.node_related_landing_pad_edges[v]:
-                    self.model.addConstr(self.is_edge_used[i][edge] + self.is_edge_used[i][jump_edge] <= 1) # only one can be true
+        for edge in self.all_edges:
+            u, v, direction = edge
+            
+            # no landing pad if edge is used
+            for jump_edge in self.node_related_landing_pad_edges[v]:
+                self.model.addConstr(self.is_edge_used[edge] + self.is_edge_used[jump_edge] <= 1) # only one can be true
 
-                # no starting pad at wrong direction if edge is used
-                for jump_edge in self.node_related_starting_pad_edges[v]:
-                    u2, v2, jump_direction = jump_edge
-                    # skip if correct direction
-                    if jump_direction == direction:
-                        continue
-                    self.model.addConstr(self.is_edge_used[i][edge] + self.is_edge_used[i][jump_edge] <= 1) # only one can be true
+            # no starting pad at wrong direction if edge is used
+            for jump_edge in self.node_related_starting_pad_edges[v]:
+                u2, v2, jump_direction = jump_edge
+                # skip if correct direction
+                if jump_direction == direction:
+                    continue
+                self.model.addConstr(self.is_edge_used[edge] + self.is_edge_used[jump_edge] <= 1) # only one can be true
 
     def generate_and_add_borders(self) -> None:
         # collect all border nodes
@@ -171,80 +187,77 @@ class Router:
             component.add_constraints(self)
 
     def add_source_node_constraints(self, node:Node, direction:Direction):
-        for i in range(self.num_nets):
-            # inflow: except in opposite direction
-            for edge in self.node_in_flow_edges[node]:
-                if edge[2] == (-direction[0], -direction[1]):
-                    self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # inflow: except in opposite direction
+        for edge in self.node_in_flow_edges[node]:
+            if edge[2] == (-direction[0], -direction[1]):
+                self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # outflow: except in opposite direction
-            for edge in self.node_out_flow_edges[node]:
-                if edge[2] == (-direction[0], -direction[1]):
-                    self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # outflow: except in opposite direction
+        for edge in self.node_out_flow_edges[node]:
+            if edge[2] == (-direction[0], -direction[1]):
+                self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # start pad: only in direction
-            for edge in self.node_related_starting_pad_edges[node]:
-                if edge[2] != direction:
-                    self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # start pad: only in direction
+        for edge in self.node_related_starting_pad_edges[node]:
+            if edge[2] != direction:
+                self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # landing pad: no
-            for edge in self.node_related_landing_pad_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # landing pad: no
+        for edge in self.node_related_landing_pad_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
     def add_sink_node_constraints(self, node:Node, input_node:Node, direction:Direction):
-        for i in range(self.num_nets):
-            # ------ for sink node ------
+        # ------ for sink node ------
 
-            # in flow: only in direction
-            for edge in self.node_in_flow_edges[node]:
-                if edge[2] != direction:
-                    self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # in flow: only in direction
+        for edge in self.node_in_flow_edges[node]:
+            if edge[2] != direction:
+                self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # out flow: no
-            for edge in self.node_out_flow_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # out flow: no
+        for edge in self.node_out_flow_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # start pad: no
-            for edge in self.node_related_starting_pad_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # start pad: no
+        for edge in self.node_related_starting_pad_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # land pad: no
-            for edge in self.node_related_landing_pad_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
-            
-            # ------ for input node ------            
-            # in flow: except from node direction (but is covered in the above)
-            pass
+        # land pad: no
+        for edge in self.node_related_landing_pad_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
+        
+        # ------ for input node ------            
+        # in flow: except from node direction (but is covered in the above)
+        pass
 
-            # outflow: all
-            pass
+        # outflow: all
+        pass
 
-            # start pad: no
-            for edge in self.node_related_starting_pad_edges[input_node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
-            
-            # land pad: only in direction
-            for edge in self.node_related_landing_pad_edges[input_node]:
-                if edge[2] != direction:
-                    self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # start pad: no
+        for edge in self.node_related_starting_pad_edges[input_node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
+        
+        # land pad: only in direction
+        for edge in self.node_related_landing_pad_edges[input_node]:
+            if edge[2] != direction:
+                self.model.addConstr(self.is_edge_used[edge] == 0)
 
     def add_null_node_constraints(self, node:Node):
-        for i in range(self.num_nets):
-            # in flow: no
-            for edge in self.node_in_flow_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # in flow: no
+        for edge in self.node_in_flow_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # out flow: no
-            for edge in self.node_out_flow_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # out flow: no
+        for edge in self.node_out_flow_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # start pad: no
-            for edge in self.node_related_starting_pad_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # start pad: no
+        for edge in self.node_related_starting_pad_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
-            # land pad: no
-            for edge in self.node_related_landing_pad_edges[node]:
-                self.model.addConstr(self.is_edge_used[i][edge] == 0)
+        # land pad: no
+        for edge in self.node_related_landing_pad_edges[node]:
+            self.model.addConstr(self.is_edge_used[edge] == 0)
 
     def add_net(self, net_sources: List[Tuple[Component, Node, Amount]], net_sinks: List[Tuple[Component, Node, Amount]]):
         # get net sources and sinks
@@ -301,19 +314,12 @@ class Router:
         
         # draw if have solution
         if self.model.SolCount > 0:
-            used_edge = {i: [edge for edge in self.all_edges if self.is_edge_used[i][edge].X > 0.5] for i in range(self.num_nets)}
+            used_edge = {edge for edge in self.all_edges if self.is_edge_used[edge].X > 0.5}
             self.draw(used_edge)
 
-    def add_objective(self):
-        step_cost_list = []
-        jump_cost_list = []
-
-        for i in range(self.num_nets):
-            step_cost_list_i = [self.is_edge_used[i][edge] * STEP_COST for edge in self.step_edges]
-            jump_cost_list_i = [self.is_edge_used[i][edge] * JUMP_COST for edge in self.jump_edges]
-            step_cost_list += step_cost_list_i
-            jump_cost_list += jump_cost_list_i
-            
+    def add_objective(self):        
+        step_cost_list = [self.is_edge_used[edge] * STEP_COST for edge in self.step_edges]
+        jump_cost_list = [self.is_edge_used[edge] * JUMP_COST for edge in self.jump_edges]
         self.model.setObjective(quicksum(step_cost_list + jump_cost_list))
 
     def draw_solution_callback(self, model, where):
@@ -322,7 +328,7 @@ class Router:
             return
 
         if where == GRB.Callback.MIPSOL:
-            used_edge = {i: [edge for edge in self.all_edges if model.cbGetSolution(self.is_edge_used[i][edge]) > 0.5] for i in range(self.num_nets)}
+            used_edge = [edge for edge in self.all_edges if model.cbGetSolution(self.is_edge_used[edge]) > 0.5]
             self.non_blocking_draw(used_edge)
 
     def add_legend(self, ax):
@@ -340,26 +346,25 @@ class Router:
             return
 
         """Draw all step and jump edges."""
-        for i in range(self.num_nets):
-            color = self.colors[i % len(self.colors)]
+        color = self.colors[0 % len(self.colors)]
 
-            used_step_edges = [edge for edge in self.step_edges if edge in used_edge[i]]
-            used_jump_edges = [edge for edge in self.jump_edges if edge in used_edge[i]]
+        used_step_edges = [edge for edge in self.step_edges if edge in used_edge]
+        used_jump_edges = [edge for edge in self.jump_edges if edge in used_edge]
 
-            for (u, v, d) in used_step_edges:
-                ux, uy = u
-                ax.plot([ux + OFFSET, v[0] + OFFSET], [uy + OFFSET, v[1] + OFFSET], c='black', zorder=1)
-                ax.scatter(ux + OFFSET, uy + OFFSET, c=color, marker='o', s=50, edgecolors='black', zorder=2)
+        for (u, v, d) in used_step_edges:
+            ux, uy = u
+            ax.plot([ux + OFFSET, v[0] + OFFSET], [uy + OFFSET, v[1] + OFFSET], c='black', zorder=1)
+            ax.scatter(ux + OFFSET, uy + OFFSET, c=color, marker='o', s=50, edgecolors='black', zorder=2)
 
-            for (u, v, d) in used_jump_edges:
-                ux, uy = u
-                u2x, u2y = ux + d[0] * (max(abs(v[0] - ux), abs(v[1] - uy)) - 1), uy + d[1] * (max(abs(v[0] - ux), abs(v[1] - uy)) - 1)
+        for (u, v, d) in used_jump_edges:
+            ux, uy = u
+            u2x, u2y = ux + d[0] * (max(abs(v[0] - ux), abs(v[1] - uy)) - 1), uy + d[1] * (max(abs(v[0] - ux), abs(v[1] - uy)) - 1)
 
-                marker = {'(0, 1)': '^', '(0, -1)': 'v', '(1, 0)': '>', '(-1, 0)': '<'}.get(str(d), 'o')
+            marker = {'(0, 1)': '^', '(0, -1)': 'v', '(1, 0)': '>', '(-1, 0)': '<'}.get(str(d), 'o')
 
-                ax.plot([u2x + OFFSET, v[0] + OFFSET], [u2y + OFFSET, v[1] + OFFSET], c='black', zorder=1)
-                ax.scatter(ux + OFFSET, uy + OFFSET, c=color, marker=marker, s=80, edgecolors='black', zorder=2)
-                ax.scatter(u2x + OFFSET, u2y + OFFSET, c=color, marker=marker, s=80, edgecolors='black', zorder=2)
+            ax.plot([u2x + OFFSET, v[0] + OFFSET], [u2y + OFFSET, v[1] + OFFSET], c='black', zorder=1)
+            ax.scatter(ux + OFFSET, uy + OFFSET, c=color, marker=marker, s=80, edgecolors='black', zorder=2)
+            ax.scatter(u2x + OFFSET, u2y + OFFSET, c=color, marker=marker, s=80, edgecolors='black', zorder=2)
 
     def draw(self, used_edge = None):
         # set up axes
